fd3b455a0ea1d118e53d1ea6a1896fc6e601d69f
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-11-22 16:25:52 +0800

    1

---
+++ school/src/Dao/studentDao.java
@@ -0,0 +1,44 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import Model.student;
+
+public class studentDao implements impleDao{
+
+	public static void main(String[] args) {
+		student s=new student();
+		s.setName("teacher");
+		s.setAdress("taipei");
+		
+		new studentDao().add(s);
+
+	}
+
+	@Override
+	public void add(Object o) {
+		student s=(student)o;
+		String url="jdbc:mysql://localhost:3306/school";
+		String user="root";
+		String password="1234";
+		String SQL="insert into student(name,address) values(?,?)";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn= DriverManager.getConnection(url, user, password);
+            PreparedStatement ps=conn.prepareStatement(SQL);
+            ps.setString(1, s.getName());
+			ps.setString(2, s.getAdress());
+			ps.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			System.out.println("no driver");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ school/src/Model/student.java
@@ -0,0 +1,25 @@
+package Model;
+
+public class student {
+private Integer id;
+private String name;
+private String adress;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAdress() {
+	return adress;
+}
+public void setAdress(String adress) {
+	this.adress = adress;
+}
+}
