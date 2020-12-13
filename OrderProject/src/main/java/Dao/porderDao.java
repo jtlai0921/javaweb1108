0df37dd9cf4803a30acd00b361fb0728af6e2703
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-12-13 10:27:17 +0800

    1

--- OrderProject/src/main/java/Dao/porderDao.java
+++ OrderProject/src/main/java/Dao/porderDao.java
@@ -2,6 +2,7 @@ package Dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Model.Porder;
@@ -40,18 +41,57 @@ public class porderDao implements implDao{
 		}
 		
 	}
-
 	@Override
 	public String queryAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn=implDao.getDB();
+		String sql="select * from porder";
+		String show="";
+		try {
+			PreparedStatement ps=conn.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next())
+			{
+				show=show+"\tID"+rs.getInt("id")+
+						"\t桌號:"+rs.getString("desk")+
+						"\tA:"+rs.getInt("A")+
+						"\tB:"+rs.getInt("B")+
+						"\tC:"+rs.getInt("C")+
+						"\tsum:"+rs.getInt("sum")+"\n";
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return show;
 	}
 
 	@Override
 	public String querySum(int start, int end) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn=implDao.getDB();
+		String sql="select * from porder where sum>=? and sum<=?";
+		String show="";
+		try {
+			PreparedStatement ps=conn.prepareStatement(sql);
+			ps.setInt(1, start);
+			ps.setInt(2, end);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next())
+			{
+				show=show+"\tID"+rs.getInt("id")+
+						"\t桌號:"+rs.getString("desk")+
+						"\tA:"+rs.getInt("A")+
+						"\tB:"+rs.getInt("B")+
+						"\tC:"+rs.getInt("C")+
+						"\tsum:"+rs.getInt("sum")+"\n";
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return show;
 	}
+
+
 
 	@Override
 	public void update(int id, Object o) {
