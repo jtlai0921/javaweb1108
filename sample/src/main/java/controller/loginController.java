1a0ecad1e4588c41585a2ae7b6a9d42f1d8689cb
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-12-27 10:15:48 +0800

    1

---
+++ sample/src/main/java/controller/loginController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class loginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public loginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username=request.getParameter("username");
+		
+		if(username.equals("abc"))
+		{
+			response.sendRedirect("member/success.html");
+		}
+		else
+		{
+			response.sendRedirect("error.html");
+		}
+		
+	}
+
+}
